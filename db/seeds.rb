ce0a923304784276398565591d1d76e0f67bed05
Scott Payton <spayton.c@gmail.com>
2019-10-24 16:21:30 -0600

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require './app/models/question'
+require './app/models/question_poro'
 
-question_1 = Question.create(category: 10, difficulty: 'easy', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
-question_2 = Question.create(category: 20, difficulty: 'medium', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
-question_3 = Question.create(category: 30, difficulty: 'hard', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
+question_1 = Question.create(category: 10, difficulty: 'easy', question: 'test', correct_answer: 'answer', options: 'incorrect, incorrect, incorrect')
+question_2 = Question.create(category: 20, difficulty: 'medium', question: 'test', correct_answer: 'answer', options: 'incorrect, incorrect, incorrect')
+question_3 = Question.create(category: 30, difficulty: 'hard', question: 'test', correct_answer: 'answer', options: 'incorrect, incorrect, incorrect')
