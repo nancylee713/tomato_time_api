a81e36e256a90a5e11d224b2f25be971f5091cd7
nancylee713 <srlee713@gmail.com>
2019-10-23 23:15:16 -0600

    Test code
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-Authored-By: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-Authored-By: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/getter.rb
+++ app/getter.rb
@@ -2,9 +2,24 @@ require 'faraday'
 require 'pry'
 require 'json'
 require 'pp'
+require './app/models/game'
+
 response = Faraday.get('https://opentdb.com/api.php?amount=5&category=20&difficulty=easy')
 raw_data = response.body
 data = JSON.parse(raw_data)
 pp data
 
-# binding.pry
+first_question = data["results"].first
+
+{
+  {"category"=>"Mythology",
+    "difficulty"=>"easy",
+    "questions" => {
+      'question' => "In Norse mythology, Thor once dressed as a woman.",
+      'question' => "Who in Greek mythology, who led the Argonauts in search of the Golden Fleece?",
+    }
+}
+
+game = Game.new(first_question)
+
+binding.pry
