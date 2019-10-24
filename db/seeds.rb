b9e0450cd87f6c1ce168f82f9e219b1631a5012b
nancylee713 <srlee713@gmail.com>
2019-10-24 16:37:31 -0600

    Modify options attribute in seed file
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require './app/models/question'
 
-question_1 = Question.create(category: 10, difficulty: 'easy', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
-question_2 = Question.create(category: 20, difficulty: 'medium', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
-question_3 = Question.create(category: 30, difficulty: 'hard', question: 'test', correct_answer: 'answer', incorrect_answers: 'incorrect, incorrect, incorrect')
+question_1 = Question.create(category: 10, difficulty: 'easy', question: 'test', correct_answer: 'answer', options: ['answer', 'incorrect, incorrect, incorrect'])
+question_2 = Question.create(category: 20, difficulty: 'medium', question: 'test', correct_answer: 'answer', options: ['answer', 'incorrect, incorrect, incorrect'])
+question_3 = Question.create(category: 30, difficulty: 'hard', question: 'test', correct_answer: 'answer', options: ['answer', 'incorrect, incorrect, incorrect'])
