a0fa97c27ae3281e73acc80b4c8b71d05af51246
nancylee713 <srlee713@gmail.com>
2019-10-24 15:30:03 -0600

    Add question attribute to attr_reader
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/question_poro.rb
+++ app/models/question_poro.rb
@@ -6,15 +6,14 @@ require './app/serializers/question_serializer'
 # require './app/services/trivia'
 
 class QuestionPoro
-  attr_reader :category, :difficulty, :correct_answer, :options
+  attr_reader :category, :difficulty, :question, :correct_answer, :options
+
   def initialize(question_data)
     @category = question_data['category']
     @difficulty = question_data['difficulty']
     @question = question_data['question']
     @correct_answer = question_data['correct_answer']
     @options = question_data['incorrect_answers'] << @correct_answer
-    # @hash +=
-    # binding.pry
   end
 
 
