a1bacd022c0a88a2fbf4f7f5f0773bf37e042ebd
nancylee713 <srlee713@gmail.com>
2019-10-23 23:14:51 -0600

    Add question model
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-Authored-By: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-Authored-By: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.describe Question, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :category }
+    it { should validate_presence_of :difficulty }
+    it { should validate_presence_of :question }
+    it { should validate_presence_of :correct_answer }
+  end
+end
