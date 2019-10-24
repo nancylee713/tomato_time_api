c6546490db36789a46b3e7ac0b97c94ea54a0bcf
Scott Payton <spayton.c@gmail.com>
2019-10-24 12:02:37 -0600

    working data filter, need to refactor it !! : )
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/api_presenter.rb
+++ app/models/api_presenter.rb
@@ -11,20 +11,25 @@ class ApiPresenter
   def question_presenter_filter
     clean_data_hash = Hash.new(0)
     q_arr = []
-    @parsed_raw_data.each do |question_hash|
-      x = question_hash.assoc "question"
-      y = question_hash.assoc "correct_answer"
-      a = question_hash.assoc "category"
-      b = question_hash.assoc "difficulty"
-        z = ["incorrect_answers", question_hash["incorrect_answers"]]
-        # binding.pry
-      q_arr << x
-      q_arr << y
-      q_arr << a
-      q_arr << b
-      final = q_arr + z
-      binding.pry
-    end #end each
-  end#end method
+    @parsed_raw_data.each do |index|
+      @parsed_raw_data.each do |question_hash|
+        x = question_hash.assoc "question"
+        y = question_hash.assoc "correct_answer"
+        a = question_hash.assoc "category"
+        b = question_hash.assoc "difficulty"
+          z = ["incorrect_answers", question_hash["incorrect_answers"]]
+          # binding.pry
+        q_arr << x
+        q_arr << y
+        q_arr << a
+        q_arr << b
+        q_arr += z
+        final = q_arr
+        all_q = []
+        all_q << final
+        binding.pry
+      end #end each
+  end #end each
+end#end method
 
 end
