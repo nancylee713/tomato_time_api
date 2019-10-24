ef2a9c8dc24dc07a961173a875e6fae8cf56c2b1
nancylee713 <srlee713@gmail.com>
2019-10-24 14:49:45 -0600

    Add scopes to question model
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
-  validates_presence_of :category, :difficulty, :question, :correct_answer
+  validates_presence_of :category, :difficulty, :question, :correct_answer, :options
+
+  scope :category, -> (category) { where(category: category) }
+  scope :difficulty, -> (difficulty) { where(difficulty: difficulty) }
+  scope :question, -> (question) { where(question: /^#{question}/) }
 end
