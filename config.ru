434152e34c88ea273e215c426d281639fbd1eb77
Scott Payton <spayton.c@gmail.com>
2019-10-24 17:00:00 -0600

    modded files

--- config.ru
+++ config.ru
@@ -1,6 +1,8 @@
 require 'bundler'
+gem 'rack'
 Bundler.require
 
 require File.expand_path('../config/environment',  __FILE__)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 run TomatoApi
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require './app/models/question'
 require './app/models/question_poro'
+require 'rubygems'
+
 
 question_1 = Question.create(category: 10, difficulty: 'easy', question: 'test', correct_answer: 'answer', options: 'incorrect, incorrect, incorrect')
 question_2 = Question.create(category: 20, difficulty: 'medium', question: 'test', correct_answer: 'answer', options: 'incorrect, incorrect, incorrect')
