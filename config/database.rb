a4c1a0fef63dba6473aced1ecc602a97d7917f5a
nancylee713 <srlee713@gmail.com>
2019-10-23 23:13:55 -0600

    Update config setting
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-Authored-By: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-Authored-By: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,3 @@
-# connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
   :adapter  => "postgresql",
   :database => "tomato_time_api"
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,19 @@
 require "bundler"
 Bundler.require
 
-Bundler.require(Sinatra::Base.environment)
-
-require "active_support/deprecation"
-require "active_support/all"
-
+# get the path of the root of the app
 APP_ROOT = File.expand_path("..", __dir__)
 
+# require the controller(s)
+Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
+
+# require the model(s)
+Dir.glob(File.join(APP_ROOT, 'app', 'models', '*.rb')).each { |file| require file }
+
+# require database configurations
 require File.join(APP_ROOT, 'config', 'database')
 
+# configure SinatraApp settings
 class App < Sinatra::Base
   set :method_override, true
   set :root, APP_ROOT
