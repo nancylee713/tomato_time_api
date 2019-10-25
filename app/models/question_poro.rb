24c4bf7238064b19d7ea3450885247dc46c6109e
nancylee713 <srlee713@gmail.com>
2019-10-25 14:49:18 -0600

    Remove non-string fragments from question content
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/question_poro.rb
+++ app/models/question_poro.rb
@@ -1,11 +1,25 @@
+require 'pry'
 class QuestionPoro
   attr_reader :category, :difficulty, :question, :correct_answer, :options
 
   def initialize(question_data)
     @category = question_data[:category]
     @difficulty = question_data[:difficulty]
-    @question = question_data[:question]
-    @correct_answer = question_data[:correct_answer]
-    @options = question_data[:incorrect_answers] << @correct_answer
+    @question = question_clean(question_data[:question])
+    @correct_answer = question_clean(question_data[:correct_answer])
+    incorrect_answers = question_clean(question_data[:incorrect_answers])
+    @options = incorrect_answers << @correct_answer
+  end
+
+  def question_clean(data)
+    map = {'&quot;' => '', '&#039;'=> "'", '&rsquo;'=> "'"}
+
+    if data.kind_of?(Array)
+      map.each { |k,v| data.map do |str| str.gsub!(k,v) end }
+    else
+      map.each { |k,v| data.gsub!(k,v) }
+    end
+
+    data
   end
 end
