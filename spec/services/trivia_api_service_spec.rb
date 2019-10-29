78d05380447622309b4c4100731ef11cc5dd060d
nancylee713 <srlee713@gmail.com>
2019-10-28 19:08:35 -0600

    Remove pry and edit variable name
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- spec/services/trivia_api_service_spec.rb
+++ spec/services/trivia_api_service_spec.rb
@@ -1,5 +1,5 @@
 require './spec/spec_helper'
-# require './app/services/trivia_api_service'
+require './app/services/trivia_api_service'
 
 RSpec.describe TriviaApiService do
   it "can be instantiated" do
@@ -10,19 +10,18 @@ RSpec.describe TriviaApiService do
 
   it "#get_questions_by_category" do
     trivia_api_service = TriviaApiService.new
-    binding.pry
     data = trivia_api_service.get_questions(2, "History")
 
     expect(data).to be_a Array
     expect(data.count).to eq 2
 
-    member = data.first
+    first_data = data.first
 
-    expect(member).to be_a Hash
-    expect(member).to have_key(:category)
-    expect(member).to have_key(:difficulty)
-    expect(member).to have_key(:question)
-    expect(member).to have_key(:correct_answer)
-    expect(member).to have_key(:incorrect_answers)
+    expect(first_data).to be_a Hash
+    expect(first_data).to have_key(:category)
+    expect(first_data).to have_key(:difficulty)
+    expect(first_data).to have_key(:question)
+    expect(first_data).to have_key(:correct_answer)
+    expect(first_data).to have_key(:incorrect_answers)
   end
 end
